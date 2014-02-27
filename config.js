ab3fdf1bf8a2bb65de159fb570c69d3a51105c30
macrarazy2 <astre.legit@gmail.com>
2014-02-27 16:02:12 +0500

    Server: Voyage || Bot Nick and Password added.
    exports.server = 'sim.psim.us' > 'voyage.psim.us'
    exports.serverid = 'showdown' > 'voyage'
    exports.rooms = ['example room name', 'example room name 2'] > ['Lobby', 'Dun Deal\'s Place']
    exports.commandcharacter = '.' > '+'

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // If you really don't know how to do this... Run `node getserver.js URL`.
 // Fill in the URL of the client where `URL` is.
 // For example: `node getserver.js http://example-server.psim.us/`
-exports.server = 'sim.psim.us';
+exports.server = 'voyage.psim.us';
 exports.port = 8000;
 
 // This is the server id.
@@ -13,22 +13,22 @@ exports.port = 8000;
 // For example, on the Smogon server, it will say somewhere in the URL
 // ~~showdown, meaning that the server id is 'showdown'.
 // If you really don't know how to check this... run the said script above.
-exports.serverid = 'showdown';
+exports.serverid = 'voyage';
 
 // The nick and password to log in with
 // If no password is required, leave pass empty
-exports.nick = 'Example bot nick';
-exports.pass = '';
+exports.nick = 'Ƥelipper';
+exports.pass = 'shipwreck';
 
 // The rooms that should be joined.
 // Joining Smogon's Showdown's Lobby is not allowed.
-exports.rooms = ['example room name', 'another example'];
+exports.rooms = ['Lobby', 'Dun Deal\'s Place'];
 
 // The character text should start with to be seen as a command.
 // Note that using / and ! might be 'dangerous' since these are used in
 // Showdown itself.
 // Using only alphanumeric characters and spaces is not allowed.
-exports.commandcharacter = '.';
+exports.commandcharacter = '+';
 
 // Whether this file should be watched for changes or not.
 // If you change this option, the server has to be restarted in order for it to
